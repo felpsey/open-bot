1815a30a741d4e4fde6f6355a6d8b399bc59e532
Ethan Phelps <ephelpse@icloud.com>
2021-09-26 20:08:59 +0100

    checking for permissions property on command object

--- app/classes/Command.js
+++ app/classes/Command.js
@@ -1,11 +1,22 @@
 class Command {
-    constructor(name, snowflake, logic, preload = false) {
+    constructor(name, snowflake, logic, permissions, preload = false) {
         this.name = name;
         this.snowflake = snowflake;
         this.logic = logic;
+        this.permissions = permissions;
     }
 
     execute(interaction) {
+
+        // Todo: Wrap this in async function
+        if (this.permissions !== undefined) {
+            console.log('This command is restricted');
+
+            // Todo: Call middleware whether restriction is user, role or group based, pass interaction.user.roleId for example
+            // Await result from middleware, reject() will log incident and push reject embed to user
+            // 2nd line level middleware check as Discord *should* already restrict commands on registration
+        }
+
         this.logic(interaction);
     }
 }
